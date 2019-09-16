6f9f14123194ffaf12e27d85634074054a3b528b
Uzess <keonuzess@gmail.com>
2019-09-16 12:25:31 +0545

    webpack added on project

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports ={
+    entry:{
+        main:'./assets/src/js/main.js'    
+    },
+    mode:"development",
+    output:{
+        filename:"[name].js",
+        path: path.resolve(__dirname, "assets/build/js"),
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:[
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    watch: true
+}
