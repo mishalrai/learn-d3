79eaafe8e45cfbe69eda90ef656c387e2db48779
Uzess <keonuzess@gmail.com>
2019-09-16 15:27:34 +0545

    d3 selector practice

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,1 +1,17 @@
-console.log('main file is loaded');
+import * as d3 from 'd3';
+
+d3.selectAll("p").style("color", function(){
+    return `hsl(${Math.random()*360}, 100%, 50%)`;
+});
+
+d3.select('body').style("background-color", "#eee");
+
+d3.selectAll('p').style( "color", ( d, i)=>{
+    console.log(d, i);
+})
+
+d3.selectAll('p')
+    .data([4, 8, 2])
+    .style( "font-size", d =>{
+        console.log(d, 'data');
+    })
