83ff1981fe6bd4235f066397b79eb85202dafdb9
Mishal <raimuna145@gmail.com>
2019-09-18 00:41:57 +0545

    json data fetched

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
 /*----------------------------------------* 
  * LOADING DATA (.CSV .TSV .JSON) 
  *----------------------------------------*/  
+
+d3.json("data/ages.json")
+  .then( resData =>{
+    const data = resData.map( obj => ({...obj, age:+obj.age}));
+    
+    /* Selector */
+    const svg = d3.select("#chart-area").append("svg")
+      .attr("width", 400)
+      .attr("height", 400);
+
+    const circles = svg.selectAll("circle")
+      .data(data)
+
+    circles.enter()
+      .append("circle")
+        .attr("cx", function( d, i){
+          return (i*50)+25;
+        })
+        .attr("cy", 25)
+        .attr("r", function(d, i){
+          return d.age * 2;
+        })
+        .attr("fill", d=> "Richar" === d.name ?"blue":"red" );
+
+  })
+  .catch( err =>{
+    console.log(err);
+  })
