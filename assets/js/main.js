eed6a72e739ac479231d5697dc4ed75e8366bb99
Uzess <keonuzess@gmail.com>
2019-09-18 12:09:25 +0545

    scale-linear complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,24 +6,27 @@ const svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400);
 
-d3.json("data/ages.json")
+d3.json("data/buildings.json")
   .then( resData =>{
     const data = resData.map( obj => ({...obj, age:+obj.age}));
 
-    const circles = svg.selectAll("circle")
-      .data(data)
+    const y = d3.scaleLinear()
+        .domain([0, 1000])
+        .range([0, 400]);
 
-    circles.enter()
-      .append("circle")
-        .attr("cx", function( d, i){
-          return (i*50)+25;
-        })
-        .attr("cy", 25)
-        .attr("r", function(d, i){
-          return d.age * 2;
-        })
-        .attr("fill", d=> "Richar" === d.name ?"blue":"red" );
-
+    const rects = svg.selectAll("rect")
+          .data(data)
+          .enter()
+          .append("rect")
+            .attr("y", 0)
+            .attr("x", function(d, i){
+              return(i*60);
+            })
+            .attr("width", 40)
+            .attr("height", function(d){
+              return y(d.height);
+            })
+            .attr("fill", "grey")
   })
   .catch( err =>{
     console.log(err);
