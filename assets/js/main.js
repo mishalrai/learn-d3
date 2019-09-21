43da5e7a76b83e1b266840938e7da1e0cadfb6cd
Mishal <raimuna145@gmail.com>
2019-09-21 13:18:46 +0545

    on work

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
  *---------------------------------------------------*/
 const margin = { top: 30, right: 20, bottom:20, left: 10 };
 
-const width = 960 - (margin.right + margin.left);
+const width = 760 - (margin.right + margin.left);
 const height = 500 - (margin.top + margin.bottom);
 
 const svg = d3.select("#chart").append("svg")
@@ -15,28 +15,28 @@ const svg = d3.select("#chart").append("svg")
 d3.json("data/data.json")
   .then( responseData =>{
     const data = responseData.map( obj => ({...obj, profit: +obj.profit, revenue: +obj.revenue}));
-    const profit = data.map( obj => obj.profit );
-    const revenue = data.map( obj => obj.revenue);
+    const profit = data.map( d => d.profit );
+    const revenue = data.map( d=> d.revenue);
+    const months = data.map( d => d.month);
 
     const x = d3.scaleBand()
-      .domain([0, width])
+      .domain(months)
       .range([0, width])
       .paddingInner([0.3])
-      .paddingOuter([0.4]);
+      .paddingOuter([0.3]);
 
     const y = d3.scaleLinear()
       .domain([0, d3.max( revenue)])
       .range([0, height]);
 
-    console.log(data, "data from data.js file  ");  
     const rects = svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-        .attr("y", d => x( d.revenue))
-        .attr("x", d => y(d.month))
+        .attr("y", d => y(d.revenue))
+        .attr("x", d => x(d.month))
         .attr("width", x.bandwidth())
-        .attr("height", d => d.revenue)
+        .attr("height", d => y(d.revenue))
         .attr("fill", "streelblue");
     
   })
