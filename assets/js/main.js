91dc7eeb1132575785d66768dc72e560b5db79c8
Mishal <raimuna145@gmail.com>
2019-09-22 23:51:58 +0545

    file updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ const margin = { top: 30, right: 20, bottom:80, left: 80 };
 
 const width = 760 - (margin.right + margin.left);
 const height = 500 - (margin.top + margin.bottom);
+const t = d3.transition().duration(750);
+let flag = true;
 
 const svg = d3.select("#chart").append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -12,6 +14,7 @@ const svg = d3.select("#chart").append("svg")
   .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+/* Scaling */
 const x = d3.scaleBand()
   .range([0, width])
   .paddingInner([0.3])
@@ -43,17 +46,16 @@ const yLabel = svg.append("text")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(90)");
   
-
-let flag = true;
-
-
+/* Requesting for data.json */
 d3.json("data/data.json")
   .then( responseData =>{
     const data = responseData.map( obj => ({...obj, profit: +obj.profit, revenue: +obj.revenue}));
     
     d3.interval( function(){
+      const newData = flag ? data : data.slice(1) ;
+      // console.log(newData, "new data");
+      update( newData );
       flag = !flag;
-      update( data );
     }, 5000);
     update(data);
 
@@ -62,50 +64,49 @@ d3.json("data/data.json")
     console.log(error, "got error while fetching data.json file");
   })
 
+/* Update data */
+function update(data){
 
-  function update(data){
+  const value = flag ? "revenue": "profit";
 
-    const value = flag ? "revenue": "profit";
+  x.domain(data.map( d=>d.month));
+  y.domain([0, d3.max( data.map( d => d[value]) )]);
+  
+  const xAxisCall = d3.axisBottom(x);
+  const yAxisCall = d3.axisLeft(y)
+    .tickFormat( d => `$${d}`);
 
-    x.domain(data.map( d=>d.month));
-    y.domain([0, d3.max( data.map( d => d[value]) )]);
-    
-    const xAxisCall = d3.axisBottom(x);
-    const yAxisCall = d3.axisLeft(y);
+  xAxisGroup.transition(t).call(xAxisCall);
+  yAxisGroup.transition(t).call(yAxisCall);
 
-    xAxisGroup.call(xAxisCall);
-    yAxisGroup.call(yAxisCall);
+  /* Rect generator */
+  /* JOIN new data with old element */
+  const rects = svg.selectAll("rect")
+    .data(data, d=>d.month);
+  
+  /* EXIT old element not prest in new data */
+  rects.exit()
+    .attr("fill", "red") 
+    .transition(t)
+      .attr("y", y(0))
+      .attr("height", 0)
+      .remove();
 
-    /* Rect generator */
-    /* JOIN new data with old element */
-    const rects = svg.selectAll("rect")
-      .data(data)
-    
-    /* EXIT old element not prest in new data */
-    rects.exit().remove();
-    
-    /* UPDATE  old elements present in new data */
-    rects
-      .attr("y", d => {
-        const rv = y(d[value]);
-        return rv;
-      })
+  /* ENTER new elements present in new data */
+  rects.enter()
+    .append("rect")
       .attr("x", d => x(d.month))
       .attr("width", x.bandwidth())
-      .attr("height", d =>{
-        const rv =  height - y(d[value]);
-        return rv;
-      });
-
-    /* ENTER new elements present in new data */
-    rects.enter()
-      .append("rect")
-        .attr("y", d => y(d[value]))
-        .attr("x", d => x(d.month))
+      .attr("y", y(0))
+      .attr("height", 0)
+      .attr("fill", "teal")
+      .merge(rects)
+      .transition(t)
         .attr("width", x.bandwidth())
-        .attr("height", d => height - y(d[value]))
-        .attr("fill", "teal");
+        .attr("x", d => x(d.month))
+        .attr("y", d => y(d[value]))
+        .attr("height", d => height - y(d[value]));
 
-    const yLabelText = flag ? "Revenue": "Profit";
-    yLabel.text(yLabelText);
-  }
+  const yLabelText = flag ? "Revenue": "Profit";
+  yLabel.text(yLabelText);
+}
